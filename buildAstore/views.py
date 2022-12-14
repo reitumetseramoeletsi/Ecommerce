17c1bb8d8177cdfc8f04b8ebf591da1db9ec5942
reitumetse <reitumetseramoletsi@gmail.com>
2022-12-14 20:53:34 +0200

    deploy

--- buildAstore/views.py
+++ buildAstore/views.py
@@ -5,7 +5,7 @@ from .models import Category, Product
 # Creating views here.
 def all_products(request):
     products = Product.objects.all()[0:6]
-    return render(request, 'buildAstore/home.html', {'products': products})
+    return render(request, 'buildAstore/index.html', {'products': products})
 
 def about(request):
     return render(request, 'buildAstore/about.html')
